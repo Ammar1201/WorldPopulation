eee220663f44ae62290bf0b05e51fc6c64fbb3ae
Ammar1201 <aamour1201@gmail.com>
2022-11-04 15:25:55 +0200

    cities buttons shows and updates with each country

--- app.js
+++ app.js
@@ -61,7 +61,24 @@ const getCountriesPopulations = countries => {
   return populationPromises;
 };
 
-const addChartData = (countries, population) => {
+const getCitiesPopulations = cities => {
+  const populationPromises = [];
+  cities.forEach(city => {
+    const method = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        city: city
+      })
+    }
+    populationPromises.push(postData('https://countriesnow.space/api/v0.1/countries/population/cities', method));
+  });
+  return populationPromises;
+};
+
+const addChartDataCountries = (countries, population) => {
   const obj = {
     type: 'bar',
     data: {
@@ -105,6 +122,50 @@ const addChartData = (countries, population) => {
   return obj;
 };
 
+const addChartDataCities = (cities, population) => {
+  const obj = {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Population',
+            data: [],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.8)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+          },
+        ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+
+  clearBtns();
+  
+  cities.forEach((city, index) => {
+    let length = -1;
+    if(population[index] !== undefined && population[index].data !== undefined) {
+      length = population[index].data.populationCounts.length - 1;
+    }
+    if(length !== -1) {
+      obj.data.labels.push(city);
+      createBtn(city);
+      obj.data.datasets[0].data.push(population[index].data.populationCounts[length].value);
+    }
+  });
+  return obj;
+};
+
 const updateChart = chartData => {
   if(firstChart) {
     const myChart = new Chart(ctx, chartData);
@@ -123,16 +184,45 @@ const fillChartCountries = async (continent) => {
   const populationPromises = getCountriesPopulations(countries);
   const population = await Promise.all(populationPromises);
 
-  const chartData = addChartData(countries, population);
+  const chartData = addChartDataCountries(countries, population);
+  updateChart(chartData);
+};
+
+const fillChartCities = async (country) => {
+  const cities = await postData('https://countriesnow.space/api/v0.1/countries/cities', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      country: country
+    })
+  });
+  const populationPromises = getCitiesPopulations(cities.data);
+  const population = await Promise.all(populationPromises);
+  console.log(population);
+
+  const chartData = addChartDataCities(cities.data, population);
   updateChart(chartData);
 };
 
 continents.addEventListener('click', (e) => {
   const target = e.target;
-  if(target.textContent.length > 7) {
+  if(target.id === 'continents') {
     return;
   }
   fillChartCountries('https://restcountries.com/v3.1/region/'  + target.textContent.toLowerCase());
+},
+{
+  capture: true
+});
+
+btns.addEventListener('click', (e) => {
+  const target = e.target;
+  if(target.id === 'btns') {
+    return;
+  }
+  fillChartCities(target.textContent.toLowerCase());
 },
 {
   capture: true
